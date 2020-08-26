8f7411f0bb2b1748a12b94b18b0ec61a75792962
joizhang <zxlwhom@outlook.com>
2020-08-26 15:21:43 +0800

    fix: Update test dataloader.

--- test.py
+++ test.py
@@ -19,50 +19,24 @@ torch.backends.cudnn.benchmark = True
 PICKLE_FILE = "plot/{}.pickle"
 
 
-def show_metrics(args):
-    with open(PICKLE_FILE.format(args.arch), "rb") as f:
-        y_true, y_pred, y_score = pickle.load(f)
-    print(len(y_true), len(y_pred), len(y_score))
-    # Accuracy
-    acc = accuracy_score(y_true, y_pred)
-    # ROC
-    fpr, tpr, thresholds = roc_curve(y_true, y_score, drop_intermediate=False)
-    print(fpr, tpr)
-    fnr = 1 - tpr
-    # Equal error rate
-    eer = fnr[np.nanargmin(np.absolute((fnr - fpr)))]
-
-    tpr_0_01 = -1
-    tpr_0_02 = -1
-    tpr_0_05 = -1
-    tpr_0_10 = -1
-    tpr_0_20 = -1
-    tpr_0_50 = -1
-    tpr_1_00 = -1
-    tpr_2_00 = -1
-    tpr_5_00 = -1
-    for i in range(len(fpr)):
-        if fpr[i] > 0.0001 and tpr_0_01 == -1:
-            tpr_0_01 = tpr[i - 1]
-        if fpr[i] > 0.0002 and tpr_0_02 == -1:
-            tpr_0_02 = tpr[i - 1]
-        if fpr[i] > 0.0005 and tpr_0_05 == -1:
-            tpr_0_05 = tpr[i - 1]
-        if fpr[i] > 0.001 and tpr_0_10 == -1:
-            tpr_0_10 = tpr[i - 1]
-        if fpr[i] > 0.002 and tpr_0_20 == -1:
-            tpr_0_20 = tpr[i - 1]
-        if fpr[i] > 0.005 and tpr_0_50 == -1:
-            tpr_0_50 = tpr[i - 1]
-        if fpr[i] > 0.01 and tpr_1_00 == -1:
-            tpr_1_00 = tpr[i - 1]
-        if fpr[i] > 0.02 and tpr_2_00 == -1:
-            tpr_2_00 = tpr[i - 1]
-        if fpr[i] > 0.05 and tpr_5_00 == -1:
-            tpr_5_00 = tpr[i - 1]
-    roc_auc = auc(fpr, tpr)
-    metrics_template = "ACC: {:f} AUC: {:f} EER: {:f} TPR@0.01: {:f} TPR@0.10: {:f} TPR@1.00: {:f}"
-    print(metrics_template.format(acc, roc_auc, eer, tpr_0_01, tpr_0_10, tpr_1_00))
+def get_dffd_dataloader(model, args):
+    input_size = model.default_cfg['input_size']
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((input_size[1], input_size[2])),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=model.default_cfg['mean'], std=model.default_cfg['std'])
+    ])
+    mask_transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((19, 19)),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor()
+    ])
+    test_data = DffdDataset(args.data_dir, 'test', transform=transform, mask_transform=mask_transform)
+    test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=1, pin_memory=True,
+                             drop_last=False)
+    return test_loader
 
 
 def test(test_loader, model, args):
@@ -109,6 +83,52 @@ def test(test_loader, model, args):
     return top1.avg
 
 
+def show_metrics(args):
+    with open(PICKLE_FILE.format(args.arch), "rb") as f:
+        y_true, y_pred, y_score = pickle.load(f)
+    print(len(y_true), len(y_pred), len(y_score))
+    # Accuracy
+    acc = accuracy_score(y_true, y_pred)
+    # ROC
+    fpr, tpr, thresholds = roc_curve(y_true, y_score, drop_intermediate=False)
+    print(fpr, tpr)
+    fnr = 1 - tpr
+    # Equal error rate
+    eer = fnr[np.nanargmin(np.absolute((fnr - fpr)))]
+
+    tpr_0_01 = -1
+    tpr_0_02 = -1
+    tpr_0_05 = -1
+    tpr_0_10 = -1
+    tpr_0_20 = -1
+    tpr_0_50 = -1
+    tpr_1_00 = -1
+    tpr_2_00 = -1
+    tpr_5_00 = -1
+    for i in range(len(fpr)):
+        if fpr[i] > 0.0001 and tpr_0_01 == -1:
+            tpr_0_01 = tpr[i - 1]
+        if fpr[i] > 0.0002 and tpr_0_02 == -1:
+            tpr_0_02 = tpr[i - 1]
+        if fpr[i] > 0.0005 and tpr_0_05 == -1:
+            tpr_0_05 = tpr[i - 1]
+        if fpr[i] > 0.001 and tpr_0_10 == -1:
+            tpr_0_10 = tpr[i - 1]
+        if fpr[i] > 0.002 and tpr_0_20 == -1:
+            tpr_0_20 = tpr[i - 1]
+        if fpr[i] > 0.005 and tpr_0_50 == -1:
+            tpr_0_50 = tpr[i - 1]
+        if fpr[i] > 0.01 and tpr_1_00 == -1:
+            tpr_1_00 = tpr[i - 1]
+        if fpr[i] > 0.02 and tpr_2_00 == -1:
+            tpr_2_00 = tpr[i - 1]
+        if fpr[i] > 0.05 and tpr_5_00 == -1:
+            tpr_5_00 = tpr[i - 1]
+    roc_auc = auc(fpr, tpr)
+    metrics_template = "ACC: {:f} AUC: {:f} EER: {:f} TPR@0.01: {:f} TPR@0.10: {:f} TPR@1.00: {:f}"
+    print(metrics_template.format(acc, roc_auc, eer, tpr_0_01, tpr_0_10, tpr_1_00))
+
+
 def main():
     args = parse_args()
     print(args)
@@ -125,26 +145,14 @@ def main():
         model.load_state_dict(checkpoint['state_dict'])
 
         print("Initializing Data Loader")
-        transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-        mask_transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((19, 19)),
-            transforms.Grayscale(num_output_channels=1),
-            transforms.ToTensor()
-        ])
-        test_data = DffdDataset(args.data_dir, 'test', transform=transform, mask_transform=mask_transform)
-        test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=1, pin_memory=True,
-                                 drop_last=False)
+        test_loader = get_dffd_dataloader(model, args)
+
+        print("Start Testing")
         test(test_loader, model, args)
     else:
         show_metrics(args)
 
 
 if __name__ == '__main__':
-    # python test.py --data-dir /data/xinlin/mini-dffd --arch vgg16 --batch-size 100 --resume weights/vgg16_dffd.pt
+    # python test.py --data-dir /data/xinlin/dffd --arch xception --batch-size 100 --gpu 1 --resume weights/xception_dffd.pt
     main()

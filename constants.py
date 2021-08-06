410b02a1fee66039723a9b53f73f384697aab58e
joizhang <zxlwhom@outlook.com>
2021-08-06 20:23:28 +0800

    chore: Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,17 @@
 # Datasets
 FACE_FORENSICS = 'ff++'
 FACE_FORENSICS_DF = 'ff++_df'
+FACE_FORENSICS_F2F = 'ff++_f2f'
+FACE_FORENSICS_FSW = 'ff++_fsw'
+FACE_FORENSICS_NT = 'ff++_nt'
 FACE_FORENSICS_FSH = 'ff++_fsh'
 CELEB_DF = 'celeb-df'
+DEEPER_FORENSICS = 'deeperface'
 DFDC = 'dfdc'
-DFFD = 'dffd'
+
+# Manipulation type of FaceForensics++
+DF = 'Deepfakes'
+F2F = 'Face2Face'
+FSH = 'FaceShifter'
+FSW = 'FaceSwap'
+NT = 'NeuralTextures'
